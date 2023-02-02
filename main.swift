8ebdb619391b5a137b0676d2b3154798cf49456c
DJ <djkim1102@naver.com>
2023-02-02 17:40:21 +0900

    fist commit

---
+++ main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ sides:[Int]) -> Int {
+    let sidesSorted = sides.sorted()
+    let a = sidesSorted[0]
+    let b = sidesSorted[1]
+    var n = 0
+    
+    b - a < n && n <= b 
+    
+    
+    return 0
+}
