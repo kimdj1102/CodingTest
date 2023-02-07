895dfebad405f02534bb8b33ba4dd78bce24ec4a
김동준 <djkim1102@naver.com>
2023-02-07 23:45:22 +0900

    Update 두 수의 합.swift

--- 프로그래머스/Lv.0/두 수의 합.swift
+++ 프로그래머스/Lv.0/두 수의 합.swift
@@ -4,4 +4,4 @@ func solution(_ num1:Int, _ num2:Int) -> Int {
     return num1 + num2
 }
 
-//[문제링크](https://school.programmers.co.kr/learn/courses/30/lessons/120802)
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
